287a76c2d14977c96dc87c01f46fce470cc76de5
RyPettEdu <rpetting@yc.edu>
2026-01-23 06:41:55 -0700

    Add files via upload
    CSC211, Week 1, Hello World

---
+++ helloworld.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<title>Hello, World!</title>
+</head>
+
+<body>
+
+<h1>My first PHP page</h1>
+
+<?php
+echo "Hello, world!";
+?>
+
+</body>
+
+</html>
