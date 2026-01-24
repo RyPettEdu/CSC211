07200ca4c2c208e833e1bf05bfbacbb0098231b7
RyPettEdu <rpetting@yc.edu>
2026-01-24 05:59:32 -0700

    Add files via upload
    CSC211, Week 1

---
+++ comments.php
@@ -0,0 +1,20 @@
+<!Doctype HTML>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<title>Comments</title>	
+</head>
+
+<body>
+<?php
+// This is a single line comment.
+# This is also a single line comment.
+/*
+This comment can span
+multiple lines.
+*/
+?>
+</body>
+
+</html>
