f3ef1568df256d0194947eaf9071806b61c4ea50
RyPettEdu <rpetting@yc.edu>
2026-01-27 06:41:05 -0700

    Add files via upload

---
+++ variables.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<title>Assigning Variable Values</title>
+</head>
+
+<body>
+
+<?php // CSC211 - Week 2
+
+$first_name = "Ryan";
+$last_name = "Pettinger";
+
+// Address
+$street = "123 First St.";
+$city = "Prescott Valley";
+$state = "AZ";
+$zip = 86314;
+
+$phone = "123-456-7890";
+
+// Prints the information:
+print "$first_name $last_name<br> $street<br> $city, $state $zip<br> $phone";
+
+?>
+
+</body>	
+
+</html>
