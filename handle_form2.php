b24c624e53d43e2c6ce071abed89a77fa50ad261
RyPettEdu <rpetting@yc.edu>
2026-02-03 06:26:08 -0700

    Added to satisfy error display requirements.
    Separated from handle_form.php

---
+++ handle_form2.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Your Feedback</title>
+</head>
+
+<body>
+<?php // Script 3.4 handle_form.php
+
+ini_set('display_errors', 1); // Let me learn from my mistakes.
+
+// This page receives the data from feedback.html
+/* It will recive: title, first_name, last_name, email, response, 
+comments, and Submit in $_POST */
+
+// Create shorthand versions of the variables. Email is not used.
+$title = $_POST['title'];
+$first_name = $_POST['first_name'];
+$last_name = $_POST['last_name'];
+$response = $_POST['response'];
+$comments = $_POST['comments'];
+
+// Print the recived data:
+print "<p>Thank you, $title $first_name $last_name, for your comments.</p>
+<p>You stated that you found this example to be '$response' and added:<br>$comments</p>";
+
+?>
+</body>
+
+</html>
