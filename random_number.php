5d0549ebb586d4ae332e5317a908ffd6213be5f1
RyPettEdu <rpetting@yc.edu>
2026-02-04 06:03:51 -0700

    Week 3

---
+++ random_number.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Random Number</title>
+</head>
+<body>
+<?php
+$random_number = mt_rand(10, 100);
+echo "<p> The number is: $random_number</p>";
+?>
+</body>
+</html>
