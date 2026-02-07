4663627e10c8f9714c18e8fc19439ea0687a9c83
RyPettEdu <rpetting@yc.edu>
2026-02-07 05:50:37 -0700

    Week 3
    CSC211 - Week 3

---
+++ passhash.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Password Hash</title>
+</head>
+<body>
+<?php
+$password = "helloclass";
+$hashed_password = password_hash($password, PASSWORD_DEFAULT);
+echo $hashed_password;
+?>
+</body>
+</html>
