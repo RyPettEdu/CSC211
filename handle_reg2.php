6f7e8754c0789608ae79596a0771f77cc95a0501
RyPettEdu <rpetting@yc.edu>
2026-02-12 06:05:31 -0700

    Week 4
    CSC211 - Week 4

---
+++ handle_reg2.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Registration</title>
+	<style type="text/css" media="screen">
+		.error { color: red; }
+	</style>
+</head>
+<body>
+<h1>Registration Results</h1>
+<?php // Script 6.3 - handle_reg.php
+/* This script receives seven values from register.html:
+email, password, confirm, year, terms, color, submit */
+
+// Address error management to suppress errors.
+ini_set('display_errors', 0);
+
+// Flag variable to track success:
+$okay = true;
+
+// Validate the email Address:
+if (empty($_POST['email']))
+{
+	print '<p class="error">Please enter your email address.</p>';
+	$okay = false;
+}
+
+// Validate the password:
+if (empty($_POST['password']))
+{
+	print '<p class="error">Please enter your password.</p>';
+	$okay = false;
+}	
+
+// If there were no errors, print a success message:
+if ($okay)
+{
+	print '<p>You have been successfully registered (but not really).</p>';
+}
+
+?>
+</body>
+</html>
