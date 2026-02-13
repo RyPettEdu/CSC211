fccfbd0a8293c24eda68e72f5623178809f5d099
RyPettEdu <rpetting@yc.edu>
2026-02-13 05:31:53 -0700

    Week 4
    CSC211 - Week 4

---
+++ soups1.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>No Soup for You!</title>
+</head>
+<body>
+<h1>Mmm...soups</h1>
+<?php // Script 7.1 - soups1.php
+/* This script creates and prints out an array. */
+
+// Create the array:
+$soups = [
+	'Monday' => 'Clam Chowder',
+	'Tuesday' => 'White Chicken Chili',
+	'Wednesday' => 'Vegetarian',
+	'Thursday' => 'Cream of Mushroom',
+	'Friday' => 'French Onion',
+	'Saturday' => 'Chicken Noodle',
+	'Sunday' => 'Zuppa Toscana'
+];
+
+// Try to print the array:
+print "<p>$soups</p>";
+
+// Print the contents of the array:
+print_r($soups);
+
+?>
+</body>
+</html>
