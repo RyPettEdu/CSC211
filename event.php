dbc7cdb493636d90c34499a2be861e282b9fedbc
RyPettEdu <rpetting@yc.edu>
2026-02-14 08:31:16 -0700

    Week 4
    CSC211 - Week 4

---
+++ event.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Add an Event</title>
+</head>
+<body>
+<?php // Script 7.9 - event.php
+/* This script handles the event form. */
+
+// Print the text:
+print "<p>You want to add an event called <b>{$_POST['name']}</b> which takes place on: <br>";
+
+// Print each week day
+if(isset($_POST['days']) AND is_array($_POST['days']))
+{
+	foreach($_POST['days'] as $day)
+	{
+		print "$day<br>\n";
+	}
+} else
+{
+	print 'Please select at least one day for the event!';
+}
+
+// Complete the paragraph:
+print '</p>';
+?>
+</body>
+</html>
